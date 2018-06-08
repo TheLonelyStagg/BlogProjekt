197e47e021d52eceee17930b1d07a8583c716252
TheLonelyStagg <ziom9706@gmail.com>
2018-06-08 19:51:11 +0200

    Updajcik

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,9 @@
+class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml {render :xml =>@tags}
+    end
+  end
+end
