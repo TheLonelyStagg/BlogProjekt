82b66ba9e001d99219ba231f1953684cf2c5513f
TheLonelyStagg <ziom9706@gmail.com>
2018-06-10 01:09:49 +0200

    Edycjapostow(Mnozy jeszcze deklaracje powiazania tagu z postem)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,21 +2,12 @@ class PostsController < ApplicationController
   def index
     @nazwa = Blog.where("id = ?", params[:blog_id]).first.name
     @posts = Post.where("blog_id = ?", params[:blog_id])
-    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml {render :xml => @posts}
-    end
   end
 
   def new
     @post = Post.new
     @nazwa = Blog.where("id = ?", params[:blog_id]).first.name
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml {render :xml => @post}
-    end
   end
 
   def create
@@ -27,10 +18,10 @@ class PostsController < ApplicationController
     String tagi = @post.tagipostu
     tagpom =tagi.split(",")
     tagpom.each do |name|
-      @nowytag = Tag.where(tagName: name).first_or_initialize
+      @nowytag = Tag.where(tagName: name.strip).first_or_initialize
       if @nowytag.save
       end
-      @tymczasowytag = Tag.where(tagName: name).first!
+      @tymczasowytag = Tag.where(tagName: name.strip).first!
       @posttags = PostTag.new
       @posttags.update_attribute(:post_id, @post.id)
       @posttags.update_attribute(:tag_id, @tymczasowytag.id)
@@ -38,16 +29,13 @@ class PostsController < ApplicationController
     end
     czy_ok = @post.save
 
-    respond_to do |format|
-      if czy_ok
-        flash[:notice] = 'Blog zostal utworzony.'
-        format.html { redirect_to blog_posts_path  }
-        format.xml  { render :xml => @post, :status => :created, :location => @post }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
-      end
+    if czy_ok
+      flash[:notice] = 'Blog zostal utworzony.'
+      redirect_to blog_posts_path
+    else
+      render :action => "new"
     end
+
   end
 
   def show
@@ -56,12 +44,6 @@ class PostsController < ApplicationController
       @comment = @post.comments.new
       @comments = Comment.where("post_id = ?", params[:id])
       @users = User.all
-
-
-      respond_to do |format|
-        format.html # show.html.erb
-        format.xml {render :xml => @post}
-      end
   end
 
   def destroy
@@ -76,6 +58,62 @@ class PostsController < ApplicationController
       end
     end
     redirect_to blog_posts_path(params[:blog_id])
+  end
+
+  def edit
+    @nazwa = Blog.where("id = ?", params[:blog_id]).first.name
+
+    if(logged_in? && (current_user.id == Blog.find(params[:blog_id]).user.id ||  current_user.is_admin ))
+
+      @post = Post.find params[:id]
+
+      @post.tagipostu = ''
+
+      ifpierwszy = true
+
+      @post.tags.each do |tagi|
+        if (ifpierwszy)
+          @post.tagipostu = tagi.tagName
+          ifpierwszy = false
+        else
+          @post.tagipostu << ", "+tagi.tagName
+        end
+      end
+
+    else
+      redirect_to :controller =>'sessions', :action=>'new'
+    end
+
+  end
+
+  def update
+    @post = Post.find params[:id]
+
+    if(logged_in? && (current_user.id == Blog.find(params[:blog_id]).user.id ||  current_user.is_admin ))
+
+      tagi = params[:post][:tagipostu]
+
+
+      tagpom =rodzaje.split(',')
+      tagpom.each do |name|
+        @nowytag = Tag.where(tagName: name.strip).first_or_initialize
+        if @nowytag.save
+        end
+        @tymczasowytag = Tag.where(tagName: name.strip).first!
+        @posttags = PostTag.where(post_id: @post.id, tag_id: @tymczasowytag.id).first_or_initialize
+        @posttags.save
+      end
+
+      if @post.update_attributes(post_params)
+        flash[:success] = 'Post zostal zupdateowany.'
+        redirect_to blog_posts_path(@post.blog.id)
+      else
+        render :action => 'new'
+      end
+    else
+      redirect_to :controller =>'sessions', :action=>'new'
+    end
+
   end
   
   private 
