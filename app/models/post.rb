d140949149a67da9c9c3d1beef90d8a948f80dc7
TheLonelyStagg <ziom9706@gmail.com>
2018-06-15 23:32:40 +0200

    Oczyszczanie Kind i Tag przy usuwaniu blogow i postow

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,6 @@ class Post < ApplicationRecord
   has_many :tags, through: :post_tags
 
   attr_accessor :tagipostu
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
